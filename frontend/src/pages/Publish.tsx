4cc625022a6799687a76b0e0e8bd8ad67da143ae
Sumit Kumar <sumit182003@gmail.com>
2024-07-20 22:15:30 +0530

    erros de digitação

--- frontend/src/pages/Publish.tsx
+++ frontend/src/pages/Publish.tsx
@@ -28,6 +28,8 @@ const Publish = () => {
                             Authorization: `Bearer ${localStorage.getItem("token")}`
                         }
                     })
+                    console.log(response.data);
+                    
                     navigate(`/blogs`)
                 }} type="submit" className="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
                     Publish post
